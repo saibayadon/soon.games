a24f8ac809fa8ea83967651479c0c555754bc0eb
Bruno Lazzaro <bruno.lazzaro@rga.com>
2018-02-01 17:40:37 -0600

    fix: add cache

--- lambda/index.js
+++ lambda/index.js
@@ -1,15 +1,28 @@
 // Requires
 import { PLATFORMS, TYPE, getGames } from './scraper'
+import moment from 'moment'
+
+// In-memory cache (Useful for burst of multiple requests when the lambda instance is still hot)
+const cache = {date: moment(), data: new Map()}
 
 // Handler
-exports.scrape = async (event, context, callback) => {
+exports.scrape = async (event = {}, context = {}, callback = () => {}) => {
   let platform = event && event.queryStringParameters ? PLATFORMS[event.queryStringParameters.platform || PLATFORMS.SWITCH] : PLATFORMS.SWITCH
   let type = event && event.queryStringParameters ? TYPE[event.queryStringParameters.type || TYPE.NEW] : TYPE.NEW
+  let key = `${platform}-${type}`
 
-  try {
-    let games = await getGames(platform, type)
-    callback(null, { "statusCode": 200, "body": JSON.stringify(games), "headers": {"Access-Control-Allow-Origin": "*"} })
-  } catch (e) {
-    callback(null, { "statusCode": 500, "body": e.message, "headers": {"Access-Control-Allow-Origin": "*"} })
+  // Check cache. Invalidate if it's older than 1 day or data is not there.
+  if (cache.date.isBefore(cache.date.clone().add(1, 'days')) && cache.data.has(key)) {
+    console.log(`${cache.date.format('X')}: Retrieved ${key} from cache.`)
+    callback(null, { "statusCode": 200, "body": JSON.stringify(cache.data.get(key)), "headers": {"Access-Control-Allow-Origin": "*"} })
+  } else {
+    try {
+      let games = await getGames(platform, type)
+      cache.data.set(key, games)
+      console.log(`${cache.date.format('X')}: Retrieved ${key} from network.`)
+      callback(null, { "statusCode": 200, "body": JSON.stringify(games), "headers": {"Access-Control-Allow-Origin": "*"} })
+    } catch (e) {
+      callback(null, { "statusCode": 500, "body": e.message, "headers": {"Access-Control-Allow-Origin": "*"} })
+    }
   }
 }
