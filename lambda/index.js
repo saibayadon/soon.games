3a9c4453f9f141fa18a76cf9657d4404f1214246
Bruno Lazzaro <bruno.lazzaro@rga.com>
2018-02-01 17:57:45 -0600

    fix: proper key handling

--- lambda/index.js
+++ lambda/index.js
@@ -6,9 +6,10 @@ import moment from 'moment'
 const cache = {date: moment(), data: new Map()}
 
 // Handler
-exports.scrape = async (event = {}, context = {}, callback = () => {}) => {
-  let platform = event && event.queryStringParameters ? PLATFORMS[event.queryStringParameters.platform || PLATFORMS.SWITCH] : PLATFORMS.SWITCH
-  let type = event && event.queryStringParameters ? TYPE[event.queryStringParameters.type || TYPE.NEW] : TYPE.NEW
+export const scrape = async (event = {}, context = {}, callback = () => {}) => {
+  // Generate the chache key + parameters for the scraper.
+  let platform = (event && event.queryStringParameters) ? PLATFORMS[event.queryStringParameters.platform || "SWITCH"] : PLATFORMS.SWITCH
+  let type = (event && event.queryStringParameters) ? TYPE[event.queryStringParameters.type || "NEW"] : TYPE.NEW
   let key = `${platform}-${type}`
 
   // Check cache. Invalidate if it's older than 1 day or data is not there.
@@ -17,12 +18,18 @@ exports.scrape = async (event = {}, context = {}, callback = () => {}) => {
     callback(null, { "statusCode": 200, "body": JSON.stringify(cache.data.get(key)), "headers": {"Access-Control-Allow-Origin": "*"} })
   } else {
     try {
+      // Get the data
       let games = await getGames(platform, type)
+
+      // Store cache + log.
       cache.data.set(key, games)
       console.log(`${cache.date.format('X')}: Retrieved ${key} from network.`)
+
+      // Return
       callback(null, { "statusCode": 200, "body": JSON.stringify(games), "headers": {"Access-Control-Allow-Origin": "*"} })
     } catch (e) {
       callback(null, { "statusCode": 500, "body": e.message, "headers": {"Access-Control-Allow-Origin": "*"} })
     }
   }
+
 }
